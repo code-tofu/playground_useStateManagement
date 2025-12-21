934a514792e3631d517af9390132d0ca1f39c15d
benjaminfoo <benjamin.ftc@gmail.com>
2025-12-21 22:05:32 +0800

    refactor App Layout

--- use-redux/src/App.tsx
+++ use-redux/src/App.tsx
@@ -1,37 +1,9 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import { SimpleGrid, Flex, Center, VStack,Heading } from "@chakra-ui/react";
 import "./App.css";
-import data from "../db.json";
-import Post from "./components/Post";
-import PostInput from "./components/PostInput";
+import AppLayout from "./AppLayout";
 
 function App() {
     return (
-        <Center>
-            <VStack>
-                <div>
-                    <VStack margin={5}>
-                    <Flex gap="4">
-                        <img src={viteLogo} className="logo" alt="Vite logo" />
-                        <img
-                            src={reactLogo}
-                            className="logo react"
-                            alt="React logo"
-                        />
-                    </Flex>
-                    <Heading >use Redux-Sagas</Heading>
-                    <Heading size="md">Vite + React</Heading>
-                    </VStack>
-                </div>
-                    <PostInput/>    
-                <SimpleGrid columns={2} gap="40px">
-                    {data.posts.map((post) => (
-                        <Post key={post.id} {...post} />
-                    ))}
-                </SimpleGrid>
-            </VStack>
-        </Center>
+        <AppLayout/>
     );
 }
 
